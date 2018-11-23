e0f293f914b1b8b0f5c42c7469297d57c9580a50
Bluexin <bluexin.games@gmail.com>
2018-11-23 13:29:28 +0100

    Add kdoc for the third actor prototype

--- src/main/kotlin/be/bluexin/drpc4k/jna/RPCActor3.kt
+++ src/main/kotlin/be/bluexin/drpc4k/jna/RPCActor3.kt
@@ -28,6 +28,42 @@ import mu.KotlinLogging
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
+/**
+ * Start a new Discord RPC Actor in the current [this] [CoroutineScope].
+ *
+ * The newly created actor will receive messages of type [RPCInputMessage] and send [RPCOutputMessage] to the
+ * specified [output].
+ *
+ * A typical usage looks like this :
+ * ```
+ * val rpcOutput = Channel<RPCOutputMessage>(capacity = Channel.UNLIMITED)
+ * val rpcInput = rpcActor3(rpcOutput)
+ * // Connect to the client via RPC
+ * rpcInput.send(RPCInputMessage.Connect(myClientKey))
+ * // Update rich presence
+ * rpcInput.send(RPCInputMessage.UpdatePresence(myPresence))
+ * // Set up receiving of updates from the RPC actor
+ * launch {
+ *     for (msg in rpcOutput) with(msg) {
+ *         when (this) {
+ *             is RPCOutputMessage.Ready -> with(user) { logger.info("Logged in as $username#$discriminator") }
+ *             is RPCOutputMessage.Disconnected -> logger.warn("Disconnected: #$errorCode $message")
+ *             is RPCOutputMessage.Errored -> logger.error("Error: #$errorCode $message")
+ *         }
+ *     }
+ * }
+ * ...
+ * // Disconnect from the client
+ * rpcInput.close()
+ * ```
+ *
+ * Note that because we use a [Channel.UNLIMITED] capacity channel, it is safe to use non-suspending [Channel.offer]
+ * instead of the suspending [Channel.send].
+ *
+ * @param output Channel the RPC Actor will be sending [RPCOutputMessage] update messages to.
+ * @param context additional to [CoroutineScope.coroutineContext] context of the coroutine.
+ * @see CoroutineScope.actor for more technical information.
+ */
 @ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
 fun CoroutineScope.rpcActor3(output: SendChannel<RPCOutputMessage>, context: CoroutineContext = EmptyCoroutineContext):
@@ -35,7 +71,19 @@ fun CoroutineScope.rpcActor3(output: SendChannel<RPCOutputMessage>, context: Cor
     RPCActor3(this, channel, output)()
 }
 
+/**
+ * Superclass for messages sent to the RPC actor.
+ */
+@Suppress("MemberVisibilityCanBePrivate")
 sealed class RPCInputMessage {
+    /**
+     * Make the RPC actor connect to the client.
+     *
+     * @param clientId your app's client ID.
+     * @param autoRegister whether Discord should register your app for automatic launch (untested! Probably broken because Java).
+     * @param steamId your app's Steam ID, if any.
+     * @param refreshRate the rate in milliseconds at which this handler will run callbacks and send info to discord.
+     */
     class Connect(
             val clientId: String,
             val autoRegister: Boolean = false,
@@ -43,18 +91,71 @@ sealed class RPCInputMessage {
             val refreshRate: Long = 500L
     ) : RPCInputMessage()
 
+    /**
+     * Update the user's Rich Presence.
+     * The presence will be cached if used before the app has connected, and automatically sent once ready.
+     *
+     * @param presence presence to update.
+     * @see DiscordRichPresence for all available options.
+     */
     class UpdatePresence(val presence: DiscordRichPresence) : RPCInputMessage()
 }
 
+/**
+ * Superclass for messages sent by the RPC actor.
+ */
+@Suppress("MemberVisibilityCanBePrivate")
 sealed class RPCOutputMessage {
+    /**
+     * Sent when the RPC actor has logged in, and is ready to be accessed.
+     */
     class Ready(val user: DiscordUser) : RPCOutputMessage()
+
+    /**
+     * Sent when the RPC actor has been disconnected.
+     *
+     * @param errorCode the error code causing disconnection.
+     * @param message the message for disconnection.
+     */
     class Disconnected(val errorCode: Int, val message: String) : RPCOutputMessage()
+
+    /**
+     * Sent when the RPC actor has detected an error.
+     *
+     * @param errorCode the error code causing the error.
+     * @param message the message for the error.
+     */
     class Errored(val errorCode: Int, val message: String) : RPCOutputMessage()
+
+    /**
+     * Sent when the someone accepted a game invitation.
+     *
+     * @param joinSecret the game invitation secret.
+     */
     class JoinGame(val joinSecret: String) : RPCOutputMessage()
+
+    /**
+     * Sent when the someone accepted a game spectating invitation.
+     *
+     * @param spectateSecret the game spectating secret.
+     */
     class Spectate(val spectateSecret: String) : RPCOutputMessage()
+
+    /**
+     * Sent when the someone requested to join the game.
+     *
+     * @param user the requester.
+     */
     class JoinRequest(val user: DiscordUser) : RPCOutputMessage()
 }
 
+/**
+ * RPC Actor implementation.
+ *
+ * @param scope the scope for this actor to act in.
+ * @param input the actor's input channel.
+ * @param output the actor's output channel.
+ */
 @ExperimentalCoroutinesApi
 private class RPCActor3(
         private val scope: CoroutineScope,
@@ -68,18 +169,26 @@ private class RPCActor3(
     private lateinit var user: DiscordUser
     private var queuedPresence: DiscordRichPresence? = null
 
+    /**
+     * Start the actor.
+     */
     suspend operator fun invoke() {
         for (m in input) onReceive(m)
     }
 
     private suspend fun onReceive(msg: RPCInputMessage) {
         when (msg) {
-            is RPCInputMessage.Connect -> with(msg) { start(clientId, autoRegister, steamId, refreshRate) }
+            is RPCInputMessage.Connect -> with(msg) { connect(clientId, autoRegister, steamId, refreshRate) }
             is RPCInputMessage.UpdatePresence -> if (initialized) DiscordRpc.Discord_UpdatePresence(msg.presence) else queuedPresence = msg.presence
         }
     }
 
-    private suspend fun start(clientId: String, autoRegister: Boolean = false, steamId: String? = null, refreshRate: Long = 500L) {
+    /**
+     * Connect the actor to the RPC Client.
+     *
+     * @see DiscordRpc.Discord_Initialize
+     */
+    private suspend fun connect(clientId: String, autoRegister: Boolean = false, steamId: String? = null, refreshRate: Long = 500L) {
         try {
             DiscordRpc.Discord_Initialize(clientId, handlers, autoRegister, steamId)
             initialized = true
